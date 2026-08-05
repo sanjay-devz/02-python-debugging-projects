922d86979f9f65f9bb4e6013c3f2215d8e2f0582
sanjay-devz <sanjay.gridz@gmail.com>
2026-08-05 13:26:56 +0530

    new for loop checked

--- 01-basic-debugging/13-step_into_demo.py
+++ 01-basic-debugging/13-step_into_demo.py
@@ -1,2 +1,5 @@
-for i in range(1,20000000000000):
-    print(f"{i} is just {i}")
+# for i in range(1,20000000000000):
+#     print(f"{i} is just {i}")
+
+for j in range(1,2001):
+    print(f"{j} is {j}")
